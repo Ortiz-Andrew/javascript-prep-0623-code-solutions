133e1aa01f57828d3b31b62b2f49ec7e79efe3e0
Andrew Ortiz <steelpreyer@gmail.com>
2023-06-24 05:24:58 +0000

    completed prep-javascript-functions

---
+++ prep-javascript-functions/main.js
@@ -0,0 +1,55 @@
+function addTwoNumbers(num1, num2) {
+  return 2 + 2;
+}
+const addTwoNumbersResult = addTwoNumbers(2, 2);
+console.log(addTwoNumbersResult);
+
+function convertHoursToMinutes(hours) {
+  return 2 * 60;
+}
+const convertHoursToMinutesResult = convertHoursToMinutes(2);
+console.log(convertHoursToMinutesResult);
+
+function getGreeting(name) {
+  const Greeting = 'Hello' + ' ' + name + '!';
+  return Greeting;
+}
+const getGreetingResult = getGreeting('World');
+console.log(getGreetingResult);
+
+function addAndMultiplyB5(num1, num2) {
+  return (10 + 5) * 5;
+}
+const addAndMultiplyB5Result = addAndMultiplyB5(10, 5);
+console.log(addAndMultiplyB5Result);
+
+function multiplyAndDivideBy5(num1, num2) {
+  return (35 * 10) / 5;
+}
+const multiplyAndDivideBy5Result = multiplyAndDivideBy5(35, 10);
+console.log(multiplyAndDivideBy5Result);
+
+function subtractTwoNumbers(num1, num2) {
+  return 22 - 7;
+}
+const subtractTwoNumbersResult = subtractTwoNumbers(22, 7);
+console.log(subtractTwoNumbersResult);
+
+function getCircleCircumference(radius) {
+  return 5 * 2 * 3.14;
+}
+const getCircleCircumferenceResult = getCircleCircumference(5);
+console.log(getCircleCircumferenceResult);
+
+function getFullName(firstName, lastName) {
+  const fullName = firstName + ' ' + lastName;
+  return fullName;
+}
+const getFullNameResult = getFullName('Juan', 'Ramirez');
+console.log(getFullNameResult);
+
+function cube(number) {
+  return 5 * 5 * 5;
+}
+const cubeResult = cube(5);
+console.log(cubeResult);
