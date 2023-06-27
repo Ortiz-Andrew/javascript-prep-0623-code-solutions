885c0a003031ee4330940b2e65279287df7390ff
Andrew Ortiz <steelpreyer@gmail.com>
2023-06-27 21:00:58 +0000

    fixed errors for prep-javascript-functions

--- prep-javascript-functions/main.js
+++ prep-javascript-functions/main.js
@@ -5,51 +5,57 @@ const addTwoNumbersResult = addTwoNumbers(2, 2);
 console.log(addTwoNumbersResult);
 
 function convertHoursToMinutes(hours) {
-  return 2 * 60;
+  const result = hours * 60;
+  return result;
 }
-const convertHoursToMinutesResult = convertHoursToMinutes(2);
-console.log(convertHoursToMinutesResult);
+const minutes = convertHoursToMinutes(2);
+console.log(minutes);
 
 function getGreeting(name) {
-  const Greeting = 'Hello' + ' ' + name + '!';
-  return Greeting;
+  const result = 'Hello' + ' ' + name + '!';
+  return result;
 }
-const getGreetingResult = getGreeting('World');
-console.log(getGreetingResult);
+const greeting = getGreeting('World');
+console.log(greeting);
 
 function addAndMultiplyB5(num1, num2) {
-  return (10 + 5) * 5;
+  const result = (num1, num2) * 5;
+  return result;
 }
 const addAndMultiplyB5Result = addAndMultiplyB5(10, 5);
 console.log(addAndMultiplyB5Result);
 
 function multiplyAndDivideBy5(num1, num2) {
-  return (35 * 10) / 5;
+  const result = (num1 * num2) / 5;
+  return result;
 }
 const multiplyAndDivideBy5Result = multiplyAndDivideBy5(35, 10);
 console.log(multiplyAndDivideBy5Result);
 
 function subtractTwoNumbers(num1, num2) {
-  return 22 - 7;
+  const result = num1 - num2;
+  return result;
 }
 const subtractTwoNumbersResult = subtractTwoNumbers(22, 7);
 console.log(subtractTwoNumbersResult);
 
 function getCircleCircumference(radius) {
-  return 5 * 2 * 3.14;
+  const result = 2 * Math.PI * radius;
+  return result;
 }
 const getCircleCircumferenceResult = getCircleCircumference(5);
 console.log(getCircleCircumferenceResult);
 
 function getFullName(firstName, lastName) {
-  const fullName = firstName + ' ' + lastName;
-  return fullName;
+  const result = firstName + ' ' + lastName;
+  return result;
 }
 const getFullNameResult = getFullName('Juan', 'Ramirez');
 console.log(getFullNameResult);
 
 function cube(number) {
-  return 5 * 5 * 5;
+  const result = number * number * number;
+  return result;
 }
 const cubeResult = cube(5);
 console.log(cubeResult);
